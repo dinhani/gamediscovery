49e8da6dadb972a0fcfd2a88000d14cd6d8e366b
Renato Dinhani <renatodinhani@gmail.com>
2018-01-25 04:16:18 -0200

    Removing app scripts

--- workspace/3-site/scripts/app/app-iptables.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Port 80 to 8080"
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
--- workspace/3-site/scripts/app/app-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export GD_ENVIRONMENT=production
-export NEO4J_PATH=./database
-
-java -server -XX:+UseCompressedOops -Xmx768M -jar ./jetty-runner.jar ./gamediscovery-site-1.0-SNAPSHOT.war &>/dev/null
