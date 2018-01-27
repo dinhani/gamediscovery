c15395b02307e97a8844b2a2be329c883793954e
Renato Dinhani <renatodinhani@gmail.com>
2018-01-27 17:00:16 -0200

    Fixed SH scripts file headers

--- workspace/1-datasource-parsers/dbpedia/parse_categories.sh
+++ workspace/1-datasource-parsers/dbpedia/parse_categories.sh
@@ -1,3 +1,3 @@
 # 4.900.000 lines
-echo -e "source\ttype\target\n" > $GD_DATA_PARSED/dbpedia/category_categories.tsv
+echo -e "source\ttype\ttarget" > $GD_DATA_PARSED/dbpedia/category_categories.tsv
 parallel --progress --pipepart --block 100m -a $GD_DATA_RAW/dbpedia_categories.ttl perl parse_categories.pl >> $GD_DATA_PARSED/dbpedia/category_categories.tsv
