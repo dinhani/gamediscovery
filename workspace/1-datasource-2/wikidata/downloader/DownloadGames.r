dbb758a1e88dc7a78df441899f79957a28d420c1
Renato Dinhani <renatodinhani@gmail.com>
2018-05-06 04:48:56 -0300

    Refactoring Wikidata downloader

--- workspace/1-datasource-2/wikidata/downloader/DownloadGames.r
+++
@@ -1,3 +0,0 @@
-DownloadGames = function(additionalEntityPrefix = "", additionalEntityCondition = "", entityLink = FALSE){
-  DownloadRelationships("Game", "wdt:P31 wd:Q7889", additionalEntityPrefix, additionalEntityCondition, entityLink)
-}
---
+++ workspace/1-datasource-2/wikidata/downloader/functions/DownloadRelationshipsOfGames.r
@@ -0,0 +1,3 @@
+DownloadRelationshipsOfGames = function(additionalEntityPrefix = "", additionalEntityCondition = "", entityLink = FALSE){
+  DownloadRelationships("Game", "wdt:P31 wd:Q7889", additionalEntityPrefix, additionalEntityCondition, entityLink)
+}
